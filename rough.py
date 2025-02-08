8626468de25e441b25d90e5cb3805ec3fd922ec7
pritamroy0990 <pritamroy0990@gmail.com>
2025-02-08 10:31:53 +0600

    class & obj

---
+++ rough.py
@@ -0,0 +1,13 @@
+lst = [1,2,3]
+my_str = "mlops playlist"
+my_int = 155
+
+# print(type(my_int))
+# lst.capitalize()
+# my_str = my_str.capitalize()
+
+# print(lst)
+
+# a = 'x'
+# b = 'y'
+# print(a+b)
